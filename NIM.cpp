ceaf07849eff1d36a8a594a43c56430d1f82c283
hallgrimurt13 <hallgrimurt13@ru.is>
2013-10-17 21:45:15 +0000

    Halli

--- NIM.cpp
+++ NIM.cpp
@@ -172,7 +172,7 @@ void notandi( int* hruga )
 	hruga[ hrugunumer - 1 ] -= eldspytur;
 	if( !samtals( hruga ) )
 	{
-		cout << endl << "Thu vannst.  Til hamingju!" << endl << endl;
+		cout << endl << "Thu tapar samt. Hallgrimur vinnur alltaf!" << enal << endl;
 		cout << "Sladu a einhvern lykil... " << endl;
 		getchar( );
 	}
